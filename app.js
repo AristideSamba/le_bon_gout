27e6bf946c759d81883b978588d0d81ab31c709a
AristideSamba <sambaaristide@gmail.com>
2024-11-21 11:23:44 +0100

    News commits

---
+++ app.js
@@ -0,0 +1,53 @@
+let carousel = document.querySelector('.carousel');
+let items = carousel.querySelectorAll('.list .item');
+let dots = document.querySelectorAll('.carousel .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+let firstPosition = 0;
+let lastPosition = items.length - 1;
+let autoPlay;
+const startAutoPlay = () =>{
+    clearInterval(autoPlay);
+    autoPlay = setInterval(() =>{
+        next.click();
+    }, 5000)
+}
+startAutoPlay();
+
+const setSlider = () =>{
+    let itemActiveOld = carousel.querySelector('.list .item.active');
+    if(itemActiveOld){
+        itemActiveOld.classList.remove('active');
+    }
+    items[active].classList.add('active');
+
+    let dotActiveOld = document.querySelector('.carousel .dots li.active');
+    if(dotActiveOld){
+        dotActiveOld.classList.remove('active');
+    } 
+    dots[active].classList.add('active');
+    startAutoPlay();
+}
+setSlider();
+
+ next.onclick = () =>{
+    active = active + 1 > lastPosition ? 0 : active + 1;
+    carousel.style.setProperty('--calculation', 1);
+    setSlider();
+    }
+
+ prev.onclick = () =>{
+    active = active - 1 < firstPosition ? lastPosition : active - 1;
+    carousel.style.setProperty('--calculation', -1);
+    setSlider();
+    }
+
+
+ dots.forEach((item, position) =>{
+    item.onclick = () => {
+        active = position;
+        setSlider();
+    }
+ })
---
+++ readme.md
@@ -0,0 +1,9 @@
+# LE BON GOUT
+
+![Logo du site](/asset/logo_le_bon_gout.png)
+
+## Description
+Le Bon Gout est un restaurant de type fast food qui propose une expérience culinaire autentique. Il est situé au 86 BD Paul BARRE 78580 MAULE.
+Le menu du Bon Gout est une véritable odyssée culinaire, proposant une sélection de plats issus de différentes régions européennes. Chaque plat est préparé avec des épices spécialement importées pour garantir l'authenticité.
+
+[Publication de la page](https://aristidesamba.github.io/le_bon_gout/)
