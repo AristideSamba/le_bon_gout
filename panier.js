da0d666fe803fb281d2ef9ad724b636eaf9fe626
AristideSamba <sambaaristide@gmail.com>
2024-12-06 10:33:37 +0100

    Section avis modifié sur certaines pages2

--- panier.js
+++ panier.js
@@ -2,7 +2,8 @@
 let iconCart = document.querySelector('.icon-cart');
 let closeCart = document.querySelector('.close-btn');
 let listProductHTML =document.querySelector('.listProduct');
-let listCartHTML = document.querySelector('.listCart');
+
+
 
 
 iconCart.addEventListener('click', () => {
